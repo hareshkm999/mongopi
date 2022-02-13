0fcb3d3f98f2b1e0ddb81d13e838633a30696b6f
haresh musti <hareshkm999@gmail.com>
2022-02-13 12:18:56 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Mongo_db_operations",
+    version="0.0.1",
+    description="Mongo DB Operation",
+    author="Harish Musti",
+    packages=find_packages(),
+    license="MIT"
+)
